46a76f2eb8f0f17455095cc6f3f6a4c96ffa6484
Lenindavid9 <idarragalenindavid@gmail.com>
2026-07-19 15:24:26 -0500

    Correcion de errores de vista y controladores sin acciones falta de conexiones de punto de venta con controlador principal operario

--- src/nexusgo/NexusGo.java
+++ src/nexusgo/NexusGo.java
@@ -32,33 +32,18 @@ public class NexusGo {
      */
     public static void main(String[] args) {
 
-
-
 //        ////        // 1. Se crea la Vista
         VistaInicioSesion login = new VistaInicioSesion();
 
-     // 2. Se crea el Controlador y se le vincula la vista
+        // 2. Se crea el Controlador y se le vincula la vista
         ControladorInicioSesion controlador = new ControladorInicioSesion(login);
 
-       // 3. Se hace visible el Login en el centro de la pantalla
+        // 3. Se hace visible el Login en el centro de la pantalla
         login.setSize(450, 450); // Ajusta las dimensiones según tu fondito.jpg
         login.setLocationRelativeTo(null);
         login.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
         login.setVisible(true);
-        
-        javax.swing.JFrame ventana = new javax.swing.JFrame("Probando Panel de Administración");
-        
-        PanelAdmi panel = new PanelAdmi();
-        // Llamamos al método que creaste que construye toda la vista
-        ventana.add(panel.VistaAdmi()); 
-        
-        ventana.setSize(1200, 750); // Un tamaño óptimo para que quepa todo tu diseño
-        ventana.setLocationRelativeTo(null); // Centra la ventana en la pantalla
-        ventana.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
-        ventana.setVisible(true); // Hace visible la ventana
-    }
-    
-        
 
     }
+
 }
